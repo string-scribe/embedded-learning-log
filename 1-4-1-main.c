0946d6efc9bb2623ea973ada98ad7a350c9a441f
string-scribe <2720073566@qq.com>
2026-09-26 16:27:12 +0800

    按键控制LED（开关）

---
+++ 1-4-1-main.c
@@ -0,0 +1,32 @@
+#include<regx52.h>
+#include<intrins.h>
+
+void Delay(unsigned int xms)	//@11.0592MHz
+{
+	unsigned char data i, j;
+	while(xms){
+		_nop_();
+		i = 2;
+		j = 199;
+		do
+		{
+			while (--j);
+		} while (--i);
+		xms--;
+	}
+}
+
+void main()
+{
+	while(1)
+	{
+		if(P3_1==0)
+		{
+			Delay(20);
+			while(P3_1==0)
+			Delay(20);	
+			P2_0=~P2_0;	
+		}
+
+	}
+}
